6ec5b74b3ba21c86c42cd7db2b785193d3ab752e
Sourav Sharma <souravat@outlook.com>
2021-08-04 11:15:31 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DataBase is Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(8080, () => {
+  console.log("Server is Running");
+});
---
+++ backend/readme.md
@@ -0,0 +1,1 @@
+This is the bacend part of the travel pin app
